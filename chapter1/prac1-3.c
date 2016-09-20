9df1197138deb7e857b3ba9612311d2624bc7509
Michael Mu <muyanbiao@gmail.com>
2016-09-20 23:18:34 +0800

    Add practice 1-3

---
+++ chapter1/prac1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* 当 fahr = 0, 20, ..., 300 时，分别打印华氏温度与摄氏温度对照表 */
+int main(){
+	int fahr, celsius;
+	int lower, upper, step;
+	
+	lower = 0;		/* 温度表的下限 */
+	upper = 300;	/* 温度表的上限 */
+	step = 20;		/* 步长 */
+	
+	fahr = lower;
+	printf("fahr\tcelsius\n");	
+	while (fahr <= upper){
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%4d\t%7d\n", fahr, celsius);
+		fahr += 20;
+	}
+	
+	return 0;
+	
+}
