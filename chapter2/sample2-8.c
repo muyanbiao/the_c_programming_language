7c27b39447322295d7039b194aeb3577139b09bd
Michael Mu <muyanbiao@gmail.com>
2016-10-07 23:30:31 +0800

    squeeze函数-从字符串s中删除字符c，strcat函数-将字符串t连接到字符串s的尾部

--- chapter2/sample2-8.c
+++ chapter2/sample2-8.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-/* squeeze函数：从字符串s中删除字符c */
 #define MAXLINE 1000
 
-void squeeze(char s[], int c);
+void squeeze(char s[], int c);		/* squeeze函数：从字符串s中删除字符c */
+void custom_strcat(char s[], char t[]);	/* strcat函数：将字符串t连接到字符串s的尾部。s必须有足够大的空间 */
 
 int custom_getline(char s[], int lim);
 
 int main(){
 	int c = 'a';
-	int len = 0;
-	char line[MAXLINE];
-	while((len = custom_getline(line, MAXLINE)) > 0){
-		squeeze(line, c);
-		printf("%s", line);
+	int len_line = 0;
+	int len_end = 0;
+	char line[MAXLINE/2];
+	char end[MAXLINE/2];
+
+	len_end = custom_getline(end, MAXLINE/2);	/* 读取尾部字符串 */
+
+	while((len_line = custom_getline(line, MAXLINE/2)) > 0){
+		squeeze(line, c);			/* 删除读取的每行字符串中的字符 'a' */
+		printf("After squeeze: %s", line);
+		custom_strcat(line, end);
+		printf("After strcat: %s", line);
 	}
+
+		
 	
 	return 0;
 }
@@ -27,6 +36,19 @@ void squeeze(char s[], int c){
 			s[j++] = s[i];
 
 	s[j] = '\0';
+}
+
+/* strcat函数：将字符串t连接到字符串s的尾部。s必须有足够大的空间 */
+void custom_strcat(char s[], char t[]){
+	int i, j;
+
+	i = j = 0;
+
+	while (s[i] != '\n')			/* 判断是否伪字符串s的尾部 */
+		i++;
+
+	while ((s[i++] = t[j++]) != '\0')	/* 拷贝t */
+		;
 }
 
 /* getline函数：将一行读入到s中，并返回其长度 */
