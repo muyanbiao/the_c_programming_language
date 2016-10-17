35715e57ffc25283941060af862c709d19423959
muyanbiao <muyanbiao@gmail.com>
2016-10-17 21:43:03 +0800

    invert(x, p, n)函数：将x中第p位开始的n个(二进制)位求反(即，1变成0,0变成1)，x的其余各位保持不变

---
+++ chapter2/prac2-7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* invert(x, p, n)函数：将x中第p位开始的n个(二进制)位求反(即，1变成0,0变成1)，x的其余各位保持不变 */
+unsigned invert(unsigned x, int p, int n);
+
+int main(){
+	return 0;
+} 
+
+unsigned invert(unsigned x, int p, int n){
+	
+}
