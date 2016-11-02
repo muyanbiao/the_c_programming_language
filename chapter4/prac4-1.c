28c15e99d2b7ce106705c0cd955d76ac31306828
Michael Mu <muyanbiao@gmail.com>
2016-11-02 23:07:36 +0800

    strrindex函数：返回t在s中最右边的位置，若未找到则返回-1

--- chapter4/prac4-1.c
+++ chapter4/prac4-1.c
@@ -2,7 +2,7 @@
 #define MAXLINE 1000	/* 最大输入行长度 */
 
 int custom_getline(char line[], int max);
-int strindex(char source[], char searchfor[]);
+int strrindex(char source[], char searchfor[]);
 
 char pattern[] = "ould";	/* 待查找的模式 */
 
@@ -10,13 +10,14 @@ char pattern[] = "ould";	/* 待查找的模式 */
 int main(){
 	char line[MAXLINE];
 	int found = 0;
-
+	int position = 0;
 	while (custom_getline(line, MAXLINE) > 0)
-		if (strindex(line, pattern) >= 0){
+		if ((position = strrindex(line, pattern)) >= 0){
 			printf("%s", line);
+			printf("The last found postion is: %d\n", position);
 			found++;
 		}
-
+	printf("found = %d\n", found);
 	return found;
 }
 
@@ -34,17 +35,17 @@ int custom_getline(char s[], int lim){
 	return i;
 }
 
-/* strindex函数：返回t在s中的位置，若未找到则返回-1 */
-int strindex(char s[], char t[]){
+/* strrindex函数：返回t在s中最右边的位置，若未找到则返回-1 */
+int strrindex(char s[], char t[]){
 	int i, j, k;
-
+	int max = -1;
 	for (i = 0; s[i] != '\0'; i++){
 		for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
 			;
 
 		if (k > 0 && t[k] == '\0')
-			return i;
+			max = i;
 	}
 
-	return -1;
+	return max;
 }
