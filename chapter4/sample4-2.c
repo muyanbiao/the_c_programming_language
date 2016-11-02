29fd51e21812886ab4b61cc0676bd83122b4242c
Michael Mu <muyanbiao@gmail.com>
2016-11-02 23:39:23 +0800

    atof函数：把字符串s转换成相应的双精度浮点数

---
+++ chapter4/sample4-2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAXLINE 100
+
+int custom_getline(char line[], int max);
+
+/* atof函数：把字符串s转换成相应的双精度浮点数 */
+double atof(char s[]);
+
+int main(){
+	double sum;
+	char line[MAXLINE];
+
+	sum = 0;
+	while (custom_getline(line, MAXLINE) > 0)
+		printf("\t%g\n", sum += atof(line));
+
+	return 0;
+}
+
+double atof(char s[]){
+
+	double val, power;
+	int i, sign;
+
+	for (i = 0; isspace(s[i]); i++)		/* 跳过空白符 */
+		;
+	sign  = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	
+	for (val = 0.0; isdigit(s[i]); i++)
+		val = 10.0 * val + (s[i] - '0');
+
+	if (s[i] == '.')
+		i++;
+
+	for (power = 1.0; isdigit(s[i]); i++){
+		val = 10.0 * val + (s[i] - '0');
+		power *= 10.0;
+	}
+
+	return sign * val / power;
+}
+
+/* getline()函数：将行保存到s中，并返回该行的长度 */
+int custom_getline(char s[], int lim){
+	int c, i;
+	i = 0;
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+
+	if (c == '\n')
+		s[i++] = c;
+	
+	s[i] = '\0';
+	return i;
+}
